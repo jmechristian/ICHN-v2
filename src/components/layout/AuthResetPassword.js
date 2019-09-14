6111d15d8158742598d5d56b754416ed8cdae3fb
Jamie Christian <jchristian@Jamies-iMac.local>
2019-09-14 10:02:44 -0400

    Auth Responsiveness

--- src/components/layout/AuthResetPassword.js
+++ src/components/layout/AuthResetPassword.js
@@ -8,10 +8,10 @@ const AuthResetPassword = () => {
   return (
     <div className='flex mb-4 min-h-screen'>
       <div
-        className='w-1/2 bg-gray-400 bg-auto bg-center'
+        className='md:w-1/3 xl:w-1/2 bg-gray-400 bg-auto bg-center invisible sm:visible'
         style={{ backgroundImage: 'url(' + bgImage + ')' }}
       ></div>
-      <div className='w-1/2 flex justify-center items-center flex-col'>
+      <div className='xs:w-full md:w-2/3 xl:w-1/2 p-8 flex justify-center items-center flex-col min-h-screen'>
         <ForgotPassword />
       </div>
     </div>
