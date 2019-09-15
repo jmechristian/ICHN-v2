39c262207b6b335352d4f70858d343535208fda2
JMEChristian <jmechristian@gmail.com>
2019-09-14 21:42:35 -0400

    more dashboard

--- src/components/profile/Dashboard.js
+++ src/components/profile/Dashboard.js
@@ -10,14 +10,17 @@ const Dashboard = () => {
     <div className="flex flex-grow flex-col">
       <DashboardHeader username="Jamie Christian" />
       <div className="flex flex-grow flex-col bg-gray-200">
-        <div className="w-full p-8">
+        <div className="w-full mt-16 px-8 mb-4">
+          <div className="text-2xl font-semibold text-gray-800 ml-6 mb-12">
+            User Dashboard
+          </div>
           <div className="flex flex-grow justify-center">
             <ItemStat title="My Items" />
             <ItemStat title="Open Items" />
             <ItemStat title="Organizations" />
           </div>
         </div>
-        <div className="w-full p-8">
+        <div className="w-full px-8 my-4">
           <div className="flex flex-grow justify-center">
             <MyItemList />
             <AvailableOrgs />
