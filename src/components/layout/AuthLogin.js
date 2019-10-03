95474594790d95d5a863fe2b121c13498c839310
JMEChristian <jmechristian@gmail.com>
2019-10-02 21:25:21 -0400

    more mobile

--- src/components/layout/AuthLogin.js
+++ src/components/layout/AuthLogin.js
@@ -11,7 +11,7 @@ const AuthLogin = () => {
         className="md:w-1/3 xl:w-1/2 bg-gray-400 bg-auto bg-center invisible sm:visible"
         style={{ backgroundImage: 'url(' + bgImage + ')' }}
       ></div>
-      <div className="max-w-full md:w-2/3 xl:w-1/2 p-8 flex justify-center items-center flex-col min-h-screen">
+      <div className="max-w-full md:w-2/3 xl:w-1/2 p-8 flex md:justify-center items-center flex-col min-h-screen">
         <Login />
       </div>
     </div>
